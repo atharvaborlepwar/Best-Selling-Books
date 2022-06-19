832eb9226ac9f49deea5626d476bbc29874bcd81
atharvaborlepwar <samarthatharva111102@gmail.com>
2022-06-19 20:01:14 +0530

    Header file started

---
+++ src/components/Header.cmp.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="header">
+      <h1>Best Selling Books</h1>
+      <input type="text" className="textField" placeholder="Search" />
+    </div>
+  );
+}
+
+
+export default Header;
